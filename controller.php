f2dc4142e3381c849bce93412a6fd96a509f67f5
Alan Hardman <alanaktion@gmail.com>
2017-09-12 21:24:59 -0600

    Show paginated results from DB, in order.

--- controller.php
+++ controller.php
@@ -14,20 +14,36 @@ class Controller extends \Controller
      */
     public function search(\Base $f3)
     {
+        $args = $f3->get("GET");
+        if (empty($args["page"])) {
+            $args["page"] = 0;
+        }
+
         $client = Base::instance()->client();
         try {
             $result = $client->search([
                 'index' => Base::INDEX_NAME,
                 'type' => 'issue',
+                'size' => 20,
+                'from' => $args["page"],
                 'body' => [
                     'query' => [
                         'match' => [
                             '_all' => $f3->get('GET.q')
                         ]
                     ]
-                ]
+                ],
             ]);
             $f3->set('result', $result);
+
+            $ids = [];
+            foreach ($result['hits']['hits'] as $hit) {
+                $ids[] = $hit['_id'];
+            }
+
+            $db = $f3->get('db.instance');
+            $issues = $db->exec('SELECT * FROM issue_detail WHERE id IN (' . implode(',', $ids) . ') ORDER BY FIELD(id,' . implode(',', $ids) . ')');
+            $f3->set('issues', $issues);
         } catch (Exception $e) {
             $f3->set('error', 'Unable to load results from Elasticsearch.');
         }
